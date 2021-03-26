f3ce7dcd790e4b6d0a7373c6c0bd8b832ac4f9fc
Kevin <korukele84@outlook.com>
2021-03-26 20:05:43 +0000

    webdriver manager implemented to manage the drivers used by the this test

--- target/classes/base.java
+++ target/classes/base.java
@@ -1,10 +1,13 @@
 package resources;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -28,14 +31,41 @@ public class base {
         fips = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
         prop.load(fips);
 
-        // Simple webdriver Set-up
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\webdriver\\chromedriver.exe");
-        ChromeOptions browserOptions = new ChromeOptions();
-        browserOptions.setCapability("browserVersion", "89.0");
-        driver = new ChromeDriver(browserOptions);
+
+        String BrowserName = prop.getProperty("browser");
+
+        switch (BrowserName){
+            case "chrome":
+                // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\webdriver\\chromedriver.exe");
+                // ChromeOptions browserOptions = new ChromeOptions();
+                //browserOptions.setCapability("browserVersion", "89.0");
+
+                // New webdriver manager implemented to manage the drivers used by the this test.
+                log.info("Starting chrome driver");
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                log.info("Starting firefox driver");
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            case "microsoft edge":
+                log.info("Starting microsoft edge driver");
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                log.info("Browser driver entered is not valid, please check and trying again.");
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
+
     }
 
 }
